8e7513150352afa93a9eb358bcd295ed2bf8127c
nafizzl <labib.lamim@gmail.com>
2023-10-17 19:32:32 -0500

    used boilerplate HTML and CSS to make example pandas DataFrame

--- Flask Web Application/clientApp_routes.py
+++ Flask Web Application/clientApp_routes.py
@@ -1,14 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+import pandas as pd
 
 path = Blueprint('routes', __name__)
 
 @path.route('/')
 def home():
-    return "Hello World!"
+    return render_template('index.html')
 
 @path.route('/admin')
 def admin():
-    return "Hello Admin!"
+    data = {
+        "Name": ["Mark Aguire", "Pat Riley", "Phil Jackson"],
+        "Age": [58, 65, 60],
+        "ID": [11235, 45947, 43673]
+    }
+    df = pd.DataFrame(data)
+    df_html = df.to_html()
+    return render_template('adminDatabase.html', table = df_html)
 
 @path.route('/<username>')
 def user(username):
