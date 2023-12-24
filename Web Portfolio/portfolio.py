559571d4c83051f6d608dd4f8100a1a6dce8102c
nafizzl <labib.lamim@gmail.com>
2023-12-24 15:37:49 -0500

    images appear after static inclusion

--- Web Portfolio/portfolio.py
+++ Web Portfolio/portfolio.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+pf = Flask(__name__)
 
-@app.route('/')
+@pf.route('/')
 def index():
     return render_template('profile.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    pf.run(debug=True)
--- Web Portfolio/tailwind.config.js
+++ Web Portfolio/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neonPink: '#ff6ac1',
+        neonBlue: '#18f1e4',
+        neonGreen: '#9eff66',
+        neonOrange: '#ff9f43',
+        neonYellow: '#f4f26d',
+        neonPurple: '#d841f4',
+        neonCyan: '#00fff9',
+        neonRed: '#ff0000',
+        neonTeal: '#00ffcc',
+        neonLime: '#bfff00',
+      }
+    },
   },
   plugins: [],
 }
