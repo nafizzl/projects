6e39e10a38f959081273c69b23a43b71db03b9a6
nafizzl <labib.lamim@gmail.com>
2023-12-30 17:54:10 -0500

    buttons work on pages

--- Web Portfolio/portfolio.py
+++ Web Portfolio/portfolio.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 pf = Flask(__name__)
 
-@pf.route('/links')
+@pf.route('/links', methods=["GET", "POST"])
 def links():
-    return render_template('profile.html')
+    if request.method == "POST":
+        buttonName = request.form.get("pfPage")
+        if buttonName == "Home":
+            return redirect(url_for("home"))
+        elif buttonName == "Links":
+            return render_template("links.html") 
+    return render_template('links.html')
 
-@pf.route('/')
-def type():
+@pf.route('/', methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        buttonName = request.form.get("pfPage")
+        if buttonName == "Home":
+            return render_template("homepage.html")
+        elif buttonName == "Links":
+            return redirect(url_for("links")) 
     return render_template('homepage.html')
 
 if __name__ == '__main__':
