a476303e916fe63a0f4b1dfbfb01fc4c5ca256b7
nafizzl <labib.lamim@gmail.com>
2023-12-29 22:33:20 -0500

    more page and buttons

--- Web Portfolio/portfolio.py
+++ Web Portfolio/portfolio.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, url_for
 
 pf = Flask(__name__)
 
-@pf.route('/')
+@pf.route('/links')
 def links():
     return render_template('profile.html')
+
+@pf.route('/')
+def type():
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
     pf.run(debug=True)
